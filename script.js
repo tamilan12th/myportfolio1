558eac36da7dec633901ac4b79b27eabb1035001
THAMARAISELVAN S V <98246832+tamilan12th@users.noreply.github.com>
2025-06-14 19:41:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// script.js
+// Animate scroll entry
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show-section");
+    }
+  });
+}, {
+  threshold: 0.1,
+});
+
+sections.forEach((section) => {
+  section.classList.add("hidden-section");
+  observer.observe(section);
+});
